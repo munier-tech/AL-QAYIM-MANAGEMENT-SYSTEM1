19479f95c496b0b284587306a8c202746cbe5904
munier-tech <muniry399@gmail.com>
2025-09-01 16:23:42 +0300

    done

--- frontend/src/pages/signUp.jsx
+++ frontend/src/pages/signUp.jsx
@@ -103,13 +103,13 @@ function Signup() {
             className="mt-6 text-3xl font-bold text-gray-900"
             variants={itemVariants}
           >
-            AL-MINHAAJ
+            AL-QIYAM
           </motion.h2>
           <motion.p 
             className="mt-2 text-lg text-gray-600"
             variants={itemVariants}
           >
-            Nidaamka Maamulka Machadka AL-MINHAAJ
+            Nidaamka Maamulka Machadka AL-QIYAM
           </motion.p>
           <motion.p 
             className="mt-2 text-sm text-gray-500"
